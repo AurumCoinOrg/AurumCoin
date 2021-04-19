PierreRochard/bitcoin

rpc, index: Add verbose amounts tracking to Coinstats index

src/node/coinstats.h
@@ -33,6 +33,13 @@ struct CCoinsStats
 
     //! The number of coins contained.
     uint64_t coins_count{0};
+
+    // Following values are only available from coinstats index
+    CAmount total_unspendable_amount{0};
+    CAmount block_unspendable_amount{0};
+    CAmount block_prevout_spent_amount{0};
+    CAmount block_new_outputs_ex_coinbase_amount{0};
+    CAmount block_coinbase_amount{0};
 };
 
 //! Calculate statistics about the unspent transaction output set

AurumCoinOrg/AurumCoin

src/node/coinstats.h
@@ -41,6 +41,17 @@ struct CCoinsStats
 
     bool from_index{false};
 
+    // Following values are only available from coinstats index
+    CAmount total_subsidy{0};
+    CAmount block_unspendable_amount{0};
+    CAmount block_prevout_spent_amount{0};
+    CAmount block_new_outputs_ex_coinbase_amount{0};
+    CAmount block_coinbase_amount{0};
+    CAmount unspendables_genesis_block{0};
+    CAmount unspendables_bip30{0};
+    CAmount unspendables_scripts{0};
+    CAmount unspendables_unclaimed_rewards{0};
+
     CCoinsStats(CoinStatsHashType hash_type) : m_hash_type(hash_type) {}
 };
 