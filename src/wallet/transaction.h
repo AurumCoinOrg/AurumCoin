bitcoin/bitcoin

wallet: don't include unconfirmed v3 txs with children in available coins

src/wallet/transaction.h
@@ -258,6 +258,10 @@ public:
     // BlockConflicted.
     std::set<Txid> mempool_conflicts;
 
+    // Track v3 mempool tx that spends from this tx
+    // so that we don't try to create another unconfirmed child
+    std::optional<Txid> v3_spend;
+
     template<typename Stream>
     void Serialize(Stream& s) const
     {

AurumCoinOrg/AurumCoin

src/wallet/transaction.h
@@ -258,6 +258,10 @@ public:
     // BlockConflicted.
     std::set<Txid> mempool_conflicts;
 
+    // Track v3 mempool tx that spends from this tx
+    // so that we don't try to create another unconfirmed child
+    std::optional<Txid> truc_child_in_mempool;
+
     template<typename Stream>
     void Serialize(Stream& s) const
     {