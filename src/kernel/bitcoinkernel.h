TheCharlatan/bitcoin

kernel: Add chainstate manager option for setting worker threads
Re-use the same pattern used for the context options. This allows users
to set the number of threads used in the validation thread pool.

src/kernel/bitcoinkernel.h
@@ -574,6 +574,18 @@ kernel_BlockManagerOptions* BITCOINKERNEL_WARN_UNUSED_RESULT kernel_block_manage
 ) BITCOINKERNEL_ARG_NONNULL(1) BITCOINKERNEL_ARG_NONNULL(2);
 
 /**
+ * @brief Set the number of available worker threads used during validation.
+ *
+ * @param[in] chainstate_manager_options Non-null, options to be set.
+ * @param[in] worker_threads The number of worker threads that should be spawned in the thread pool
+ *                           used for validation. The number should be greater than 0.
+ */
+void kernel_chainstate_manager_options_set_worker_threads_num(
+        kernel_ChainstateManagerOptions* chainstate_manager_options,
+        int worker_threads
+) BITCOINKERNEL_ARG_NONNULL(1);
+
+/**
  * Destroy the block manager options.
  */
 void kernel_block_manager_options_destroy(kernel_BlockManagerOptions* block_manager_options);

AurumCoinOrg/AurumCoin

src/kernel/bitcoinkernel.h
@@ -730,6 +730,18 @@ BITCOINKERNEL_API btck_ChainstateManagerOptions* BITCOINKERNEL_WARN_UNUSED_RESUL
     size_t blocks_directory_len) BITCOINKERNEL_ARG_NONNULL(1, 2);
 
 /**
+ * @brief Set the number of available worker threads used during validation.
+ *
+ * @param[in] chainstate_manager_options Non-null, options to be set.
+ * @param[in] worker_threads             The number of worker threads that should be spawned in the thread pool
+ *                                       used for validation. When set to 0 no parallel verification is done.
+ *                                       The value range is clamped internally between 0 and 15.
+ */
+BITCOINKERNEL_API void btck_chainstate_manager_options_set_worker_threads_num(
+    btck_ChainstateManagerOptions* chainstate_manager_options,
+    int worker_threads) BITCOINKERNEL_ARG_NONNULL(1);
+
+/**
  * Destroy the chainstate manager options.
  */
 BITCOINKERNEL_API void btck_chainstate_manager_options_destroy(btck_ChainstateManagerOptions* chainstate_manager_options);